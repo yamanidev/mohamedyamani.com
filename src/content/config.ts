d58e8f3e548b05c1d54e132deaeedb7de77cdb61
yamanidev <elamine.yamani@gmail.com>
2024-05-08 21:53:02 +0100

    Added imageAlt to schema

--- src/content/config.ts
+++ src/content/config.ts
@@ -3,14 +3,22 @@ import { defineCollection, z } from 'astro:content';
 const blog = defineCollection({
   type: 'content',
   // Type-check frontmatter using a schema
-  schema: z.object({
-    title: z.string(),
-    description: z.string(),
-    // Transform string to Date object
-    pubDate: z.coerce.date(),
-    updatedDate: z.coerce.date().optional(),
-    image: z.string().optional()
-  })
+  schema: z
+    .object({
+      title: z.string(),
+      description: z.string(),
+      // Transform string to Date object
+      pubDate: z.coerce.date(),
+      updatedDate: z.coerce.date().optional(),
+      image: z.string().optional(),
+      imageAlt: z.string().optional()
+    })
+    .refine((data) => {
+      if (data.image && !data.imageAlt) {
+        throw new Error('Missing alt property for image');
+      }
+      return true;
+    })
 });
 
 export const collections = { blog };
