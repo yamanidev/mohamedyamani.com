2f021b9170cc837b7b7b7950496d370a4d967bad
yamanidev <elamine.yamani@gmail.com>
2025-03-29 14:44:03 +0100

    feat: add Remark modified time plugin

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,8 @@
-import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
-
 import tailwind from "@astrojs/tailwind";
+import { defineConfig } from "astro/config";
+import { remarkModifiedTime } from "./remark-modified-time.mts";
 
 // https://astro.build/config
 export default defineConfig({
@@ -10,7 +10,8 @@ export default defineConfig({
   markdown: {
     shikiConfig: {
       theme: "github-dark-dimmed"
-    }
+    },
+    remarkPlugins: [remarkModifiedTime]
   },
   integrations: [mdx(), sitemap(), tailwind()]
 });
